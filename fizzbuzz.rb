f7980f4b6604e7ed131bdd45073941e2a8a24de6
Luke Conley <luke.sovern.conley@gmail.com>
2018-03-29 19:31:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,14 @@
-def fizzbuzz
-
+def fizzbuzz (int)
+ if int % 3 == 0
+   "Fizz"
+ end
+ elsif int % 5 == 0
+   "Buzz"
+ end
+ elsif int % 5 == 0 && int % 3 == 0
+   "FizzBuzz"
+ end
+ else
+   "nil"
+ end
 end
